2cd142bc4a86670b8cacd3943f65e232995fddea
LucaBrembilla <lucabremby00@gmail.com>
2022-03-21 18:40:03 +0100

    Inserimento della porta come variabile d'ambiente
    Per runnare il codice è necessario permettere la lettura di variabili d'ambiente con la flag --allow-env

--- deno/index.js
+++ deno/index.js
@@ -1,7 +1,7 @@
 import { copy } from "https://deno.land/std@0.103.0/io/util.ts";
 
 const hostname = "127.0.0.1";
-const port = 3000;
+const port = Deno.env.get("PORT") || 3000;
 
 const listener = Deno.listen({ hostname, port });
 console.log(`Listening on ${hostname}:${port}`);
