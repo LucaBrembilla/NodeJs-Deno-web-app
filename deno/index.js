259134f40df55df40c0c3a8e0aa85d79a719f202
LucaBrembilla <lucabremby00@gmail.com>
2022-03-21 18:36:51 +0100

    Create index.js

---
+++ deno/index.js
@@ -0,0 +1,11 @@
+import { copy } from "https://deno.land/std@0.103.0/io/util.ts";
+
+const hostname = "127.0.0.1";
+const port = 3000;
+
+const listener = Deno.listen({ hostname, port });
+console.log(`Listening on ${hostname}:${port}`);
+
+for await (const conn of listener) {
+	console.log("Ciao");
+}
