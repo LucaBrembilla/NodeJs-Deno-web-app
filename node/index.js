492020c60f3daca6262b80413fbcb0639d4eae8b
LucaBrembilla <lucabremby00@gmail.com>
2022-04-01 16:59:23 +0200

    Update index.js

--- node/index.js
+++ node/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 
-mongoose.connect('mongodb://localhost/filmsAPI')
+mongoose.connect('mongodb://localhost/SpotAPY')
   .then(() => console.log('Connected to MongoDB...'))
   .catch(err => console.error('Could not connect to MongoDB...'));
 
