8ba744a7fe4f6a0d67cfbf5f2574f74cf5b3dca4
LucaBrembilla <lucabremby00@gmail.com>
2022-04-02 17:45:49 +0200

    Update index.js

--- node/index.js
+++ node/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 
-mongoose.connect('mongodb://localhost/SpotAPY')
+mongoose.connect('mongodb://localhost/MusicAPI')
   .then(() => console.log('Connected to MongoDB...'))
   .catch(err => console.error('Could not connect to MongoDB...'));
 
