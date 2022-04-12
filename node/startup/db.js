4a06b0914575631155c5f38681aec0c6ddf1fc0a
LucaBrembilla <lucabremby00@gmail.com>
2022-04-12 18:01:00 +0200

    Update db.js

--- node/startup/db.js
+++ node/startup/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 module.exports = function() {
-	mongoose.connect('mongodb://localhost/vidly')
+	mongoose.connect('mongodb://localhost/musicAPI')
     .then(() => console.log('Connected to MongoDB...'))
     .catch(err => console.error('Could not connect to MongoDB...'));
 }
