a5c59e2360d5bc79ee069ecec29b8a7b4e54b907
LucaBrembilla <lucabremby00@gmail.com>
2022-04-12 21:59:18 +0200

    Update genres.js

--- node/routes/genres.js
+++ node/routes/genres.js
@@ -1,4 +1,4 @@
-const Genre = require("../models/genre");
+const {Genre} = require("../models/genre");
 const mongoose = require("mongoose");
 const express = require("express");
 const router = express.Router();
@@ -6,6 +6,13 @@ const router = express.Router();
 router.get("/", async (req, res) => {
 	const genres = await Genre.find().sort("name");
 	res.send(genres);
+});
+
+router.post("/", async (req,res) => {
+	const genre = new Genre({ name: req.body.name });
+  await genre.save();
+  
+  res.send(genre);
 });
 
 module.exports = router;
