63042da11026cd00db6bbcc7f2a8703f01c8a7c3
LucaBrembilla <lucabremby00@gmail.com>
2022-04-15 16:32:01 +0200

    Update post song

--- node/routes/songs.js
+++ node/routes/songs.js
@@ -9,11 +9,11 @@ router.get("/", async (req, res) => {
 	res.send(songs);
 });
 
-router.post('/', async (req, res) => {
-	const artist = await Artist.findById(req.body.artistId);
+router.post("/", async (req, res) => {
+	let artist = await Artist.findById(req.body.artistId[0]);
   if (!artist) return res.status(400).send('Invalid artist.');
 
-  const genre = await Genre.findById(req.body.genreId);
+  let genre = await Genre.findById(req.body.genreId);
   if (!genre) return res.status(400).send('Invalid genre.');
 
   let song = new Song({ 
@@ -28,6 +28,16 @@ router.post('/', async (req, res) => {
     },
 		releaseDate: req.body.releaseDate
   });
+	for(let i = 1; i<req.body.genreId.length; i++){
+		genre = await Genre.findById(req.body.genreId[i]);
+		if (!genre) return res.status(400).send('Invalid genre.'); 
+		song.genre.push({ _id: genre._id, name: genre.name });
+	}
+	for(let i = 1; i<req.body.artistId.length; i++){
+		artist = await Artist.findById(req.body.artistId[i]);
+		if (!artist) return res.status(400).send('Invalid artist.'); 
+		song.artist.push({ _id: artist._id, alias: artist.alias });
+	}
   await song.save();
   
   res.send(song);
