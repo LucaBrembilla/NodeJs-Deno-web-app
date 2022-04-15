3e17c2bb63e3e63648b27484b83980a07487e3e6
LucaBrembilla <lucabremby00@gmail.com>
2022-04-15 17:13:12 +0200

    Finished songs route

--- node/routes/songs.js
+++ node/routes/songs.js
@@ -13,7 +13,7 @@ router.post("/", async (req, res) => {
 	let artist = await Artist.findById(req.body.artistId[0]);
   if (!artist) return res.status(400).send('Invalid artist.');
 
-  let genre = await Genre.findById(req.body.genreId);
+  let genre = await Genre.findById(req.body.genreId[0]);
   if (!genre) return res.status(400).send('Invalid genre.');
 
   let song = new Song({ 
@@ -28,6 +28,7 @@ router.post("/", async (req, res) => {
     },
 		releaseDate: req.body.releaseDate
   });
+	
 	for(let i = 1; i<req.body.genreId.length; i++){
 		genre = await Genre.findById(req.body.genreId[i]);
 		if (!genre) return res.status(400).send('Invalid genre.'); 
@@ -40,6 +41,56 @@ router.post("/", async (req, res) => {
 	}
   await song.save();
   
+  res.send(song);
+});
+
+router.get("/:id", async (req, res) => {
+	const song = await Song.findById(req.params.id);
+  if (!song) return res.status(404).send('The song with the given ID was not found.');
+  res.send(song);
+});
+
+
+router.put('/:id', async (req, res) => {
+  let artist = await Artist.findById(req.body.artistId[0])
+  if (!artist) return res.status(400).send('Invalid artist.');
+
+	let genre = await Genre.findById(req.body.genreId[0]);
+  if (!genre) return res.status(400).send('Invalid genre.');
+
+  const song = await Song.findByIdAndUpdate(req.params.id,
+	{ 
+		title: req.body.title,
+		artist: {
+			_id: artist._id,
+			alias: artist.alias, 
+		},
+		genre: {
+			_id: genre._id,
+			name: genre.name
+		},
+		releaseDate: req.body.releaseDate
+	}, { new: true });
+	for(let i = 1; i<req.body.genreId.length; i++){
+		genre = await Genre.findById(req.body.genreId[i]);
+		if (!genre) return res.status(400).send('Invalid genre.'); 
+		song.genre.push({ _id: genre._id, name: genre.name });
+	}
+	for(let i = 1; i<req.body.artistId.length; i++){
+		artist = await Artist.findById(req.body.artistId[i]);
+		if (!artist) return res.status(400).send('Invalid artist.'); 
+		song.artist.push({ _id: artist._id, alias: artist.alias });
+	}
+	await song.save();
+
+  if (!song) return res.status(404).send('The song with the given ID was not found.');
+  
+  res.send(song);
+});
+
+router.delete('/:id', async (req, res) => {
+  const song = await Song.findByIdAndRemove(req.params.id);
+  if (!song) return res.status(404).send('The song with the given ID was not found.');
   res.send(song);
 });
 
