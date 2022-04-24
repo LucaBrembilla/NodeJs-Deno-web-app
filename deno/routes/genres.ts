2823126d1dfc5404e42eb9663ce368752b402b99
LucaBrembilla <lucabremby00@gmail.com>
2022-04-24 18:33:07 +0200

    .

--- deno/routes/genres.ts
+++ deno/routes/genres.ts
@@ -1,5 +1,6 @@
 import { Router, Context } from "../deps.ts";
 import { genresCollection } from "../startup/db.ts";
+import { GenreSchema } from "../models/genre.ts";
 
 const routerGenres = new Router;
 
@@ -12,10 +13,18 @@ routerGenres
 	.post("/", async (ctx: Context) => {
 		const { value } = await ctx.request.body();
 		const { name } = await value;
+		
 		const genre = {
 			name: name,
 			_v: 0
 		};
+		
+		if( name === null || name === undefined || typeof name !== "string" || name.length<1 || name.length>50 ){
+			ctx.response.status = 400;
+			ctx.response.body = "You typed incorrect JSON";
+			return;
+		}
+
 		await genresCollection.insertOne(genre);
 		ctx.response.status = 200;
 		ctx.response.body = genre;
